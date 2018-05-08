733ce62bbc76f56962c99fb3af4825a65bf9b8a5
Cole Pazoid <34737903+ColePZ@users.noreply.github.com>
2018-05-08 15:32:20 +0100

    Added Core Commands
    Commands

---
+++ main.py
@@ -0,0 +1,22 @@
+command = ""
+command = input("Command: ")
+
+if command == "Ping":
+ print("Pong")
+ 
+ 
+ if command == "Pizza":
+ print("Yum")
+
+
+if command == "Help":
+ print("Ping, Pizza, Help, Bananas")
+ 
+ 
+ 
+ if command == "Bananas":
+ print("Hello")
+ 
+ 
+ if command == "Ping":
+ print("Pong")
